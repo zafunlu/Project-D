f84d6611c8a80b36e41d100b274d560f1703ea71
Zafer <0970135@hr.nl>
2020-05-14 20:28:52 +0200

    Login button functionality added
    Login button loads the Overzicht scene

---
+++ Assets/Scripts/uiScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uiScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Load(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
