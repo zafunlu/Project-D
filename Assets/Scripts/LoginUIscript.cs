dda7ed9035d5f420dccba893351b374ce44b8193
ronanhub <45386974+ronanhub@users.noreply.github.com>
2020-06-11 13:49:26 +0200

    Unit tests en null error fix

--- Assets/Scripts/LoginUIscript.cs
+++ Assets/Scripts/LoginUIscript.cs
@@ -12,38 +12,55 @@ public class LoginUIscript : MonoBehaviour
     public InputField passwordField;
 
     public List<Tuple<string, string>> mockDB;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         loginButton.onClick.AddListener(loginClick);
-        mockDB = new List<Tuple<string, string>> { new Tuple<string, string>("Johan5", "HuisMus12") , new Tuple<string, string>("JelleB","WachtwoorD9") };
+        mockDB = new List<Tuple<string, string>> { new Tuple<string, string>("Johan5", "HuisMus12"), new Tuple<string, string>("JelleB", "WachtwoorD9") };
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void loginClick()
+    public bool namePasswordMatch(string name, string password, List<Tuple<string, string>> database)
     {
-        foreach(var account in mockDB)
+        if (database == null || name == null || password == null)
         {
-            string name = nameField.GetComponentInChildren<Text>().text;
-            string password = passwordField.GetComponentInChildren<Text>().text;
+            return false;
+        }
+        foreach (var account in database)
+        {
             if (account.Item1.ToLower() == name.ToLower())
             {
                 if (account.Item2 == password)
                 {
-                    SceneManager.LoadScene("Overzicht");
+                    return true;
                 }
                 else
                 {
-                    //display a warning
+                    return false;
                 }
-                break;
             }
+        }
+        return false;
+    }
+
+    void loginClick()
+    {
+        string name = nameField.GetComponentInChildren<Text>().text;
+        string password = passwordField.GetComponentInChildren<Text>().text;
+            
+        if (namePasswordMatch(name,password,mockDB))
+        {
+            SceneManager.LoadScene("Overzicht");
+        }
+        else
+        {
+            //display a warning
         }
     }
 }
--- Assets/Tests/Login.cs
+++ Assets/Tests/Login.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEditor.SceneManagement;
@@ -21,11 +22,11 @@ namespace Tests
         public IEnumerator LoginComponentsCheck()
         {
             yield return null;
-            
+
             var usernameInputObj = GameObject.Find("inputUsername");
             var passwordInputObj = GameObject.Find("inputPassword");
             var loginBtnObj = GameObject.Find("btnLogin");
-            
+
             // Test objects
             Assert.NotNull(usernameInputObj);
             Assert.NotNull(passwordInputObj);
@@ -34,13 +35,169 @@ namespace Tests
             var usernameInput = usernameInputObj.GetComponent<InputField>();
             var passwordInput = passwordInputObj.GetComponent<InputField>();
             var btn = loginBtnObj.GetComponent<Button>();
-            
+
             // Test available components
             Assert.NotNull(usernameInput);
             Assert.NotNull(passwordInput);
             Assert.NotNull(btn);
-            
+
             //TODO check other functionality when implemented
+        }
+
+        [Test]
+        public void namePasswordMatchTest1()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "Testnaam1";
+            string password = "testPW123";
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsTrue(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest2()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "tESTnaam1";
+            string password = "testPW123";
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsTrue(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest3()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "Testnaam2";
+            string password = "testPW123";
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest4()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "Testnaam1";
+            string password = "TESTpw123";
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest5()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "Testnaam1";
+            string password = "testpw123";
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest6()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "Testnaam1";
+            string password = "foutje";
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest7()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "foutnaam";
+            string password = "testPW123";
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest8()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = null;
+            string password = "testPW123";
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest9()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "Testnaam1";
+            string password = null;
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest10()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "Testnaam1";
+            string password = "testPW123";
+            List<Tuple<string, string>> mockdb = null;
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest11()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = null;
+            string password = "testPW123";
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>(null, "testPW123"), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
+        }
+
+        [Test]
+        public void namePasswordMatchTest12()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Login.unity");
+            LoginUIscript script = GameObject.Find("Canvas").GetComponent<LoginUIscript>();
+
+            string username = "Testnaam1";
+            string password = null;
+            var mockdb = new List<Tuple<string, string>> { new Tuple<string, string>("Testnaam1", null), new Tuple<string, string>("Testnaam2", "TESTpw123") };
+
+            Assert.IsFalse(script.namePasswordMatch(username, password, mockdb));
         }
     }
 }
